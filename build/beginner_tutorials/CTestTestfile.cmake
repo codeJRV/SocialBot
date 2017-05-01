b59cc5ff3e7eb06eaa1af85e6a00f4ca134c3f9c
codeJRV <email.jrv@gmail.com>
2017-05-02 01:09:23 +0530

    Talker-Listener tutorial:KO

---
+++ build/beginner_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jrv/Desktop/SocialBot/SocialBot/src/beginner_tutorials
+# Build directory: /home/jrv/Desktop/SocialBot/SocialBot/build/beginner_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
