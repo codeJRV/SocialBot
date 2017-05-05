ba7b667dbb91f17f4181c285d2852f315c17aa0b
codeJRV <email.jrv@gmail.com>
2017-05-05 10:25:33 +0530

    Adding Ros best Practices

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jrv/Desktop/SocialBot/SocialBot/src
-# Build directory: /home/jrv/Desktop/SocialBot/SocialBot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(beginner_tutorials)
--- build/beginner_tutorials/catkin_generated/installspace/beginner_tutorialsConfig.cmake
+++ build/beginner_tutorials/catkin_generated/installspace/beginner_tutorialsConfig.cmake
@@ -68,7 +68,7 @@ set(beginner_tutorials_CONFIG_INCLUDED TRUE)
 # set variables for source/devel/install prefixes
 if("FALSE" STREQUAL "TRUE")
   set(beginner_tutorials_SOURCE_PREFIX /home/jrv/Desktop/SocialBot/SocialBot/src/beginner_tutorials)
-  set(beginner_tutorials_DEVEL_PREFIX /home/jrv/Desktop/SocialBot/SocialBot/devel)
+  set(beginner_tutorials_DEVEL_PREFIX /home/jrv/Desktop/SocialBot/SocialBot/devel/.private/beginner_tutorials)
   set(beginner_tutorials_INSTALL_PREFIX "")
   set(beginner_tutorials_PREFIX ${beginner_tutorials_DEVEL_PREFIX})
 else()
@@ -122,7 +122,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/jrv/Desktop/SocialBot/SocialBot/install/lib;/opt/ros/kinetic/lib)
+    foreach(path /home/jrv/Desktop/SocialBot/SocialBot/install/lib;/home/jrv/Desktop/SocialBot/SocialBot/devel/lib;/opt/ros/kinetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
